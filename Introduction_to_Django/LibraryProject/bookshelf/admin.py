f0c2fd749df66030dd6d572f16e25291022d09a3
Ednah Bridget <bidgetkakah@gmail.com>
2025-08-31 20:33:28 +0300

    Register Book in admin with list_display, list_filter and search_fields

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # Columns to show in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+
+    # Filters shown in the right sidebar (or top depending on Django version)
+    list_filter = ('publication_year', 'author')
+
+    # Fields that can be searched using the admin search box
+    search_fields = ('title', 'author')
+
+# Register the model with the admin site
+admin.site.register(Book, BookAdmin)
