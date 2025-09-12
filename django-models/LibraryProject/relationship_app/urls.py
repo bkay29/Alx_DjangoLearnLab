aaaf04ba70845fb4b434ca64076db458521aa98a
Ednah Bridget <bidgetkakah@gmail.com>
2025-09-12 22:20:35 +0300

    Add all changes to URL configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from .views import list_books
+from .views import LibraryDetailView
 
 urlpatterns = [
     # function-based view
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
 
     # class-based view (detail for a library) — DetailView expects a PK by default
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
