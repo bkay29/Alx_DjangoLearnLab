9acc3b01c84e67e4fff66e0bcbc04a76acc82d5f
Ednah Bridget <bidgetkakah@gmail.com>
2025-09-13 11:51:43 +0300

    Fix updates to authentication in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 
+from . import views
 from .views import register
 from django.contrib.auth import views as auth_views
 
@@ -14,7 +15,7 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),     
 ]
