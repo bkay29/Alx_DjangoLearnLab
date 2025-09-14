565d963a283be93a1b7ade89900f576d711f1210
Ednah Bridget <bidgetkakah@gmail.com>
2025-09-14 13:37:33 +0300

    Add Custom Permissions to project

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,8 +21,15 @@ urlpatterns = [
         template_name='relationship_app/logout.html'
     ), name='logout'),
 
-    # Role-based views (checker requires these)
+    # Role-based views
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+
+    # ---------------------------
+    # Secured Book Management URLs
+    # ---------------------------
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
