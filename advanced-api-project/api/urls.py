b4201c35e1f17539a3e15c4583df83e9bc5be3ee
Ednah Bridget <bidgetkakah@gmail.com>
2025-09-25 13:35:16 +0300

    Add all updates to views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='books-detail'),
 
-    # ----- Explicit literal routes 
-    # both with and without trailing slash to be ultra-explicit
+
+    # create route (both forms)
+    path('books/create', views.BookCreateView.as_view(), name='books-create-no-slash'),
+    path('books/create/', views.BookCreateView.as_view(), name='books-create'),
+
+    # update/delete literal forms (both forms)
     path('books/update', views.BookUpdateView.as_view(), name='books-update-no-slash'),
     path('books/update/', views.BookUpdateView.as_view(), name='books-update-dash-slash'),
 
@@ -16,6 +20,6 @@ urlpatterns = [
     # --------------------------------------------------------------------
 
     # functional routes that actually accept a pk for update/delete operations
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='books-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='books-delete'),
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='books-update-pk'),
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='books-delete-pk'),
 ]
