e8353126ac8fc566039f5aad6ed8a7d14e6c129f
Ednah Bridget <bidgetkakah@gmail.com>
2025-09-25 13:04:37 +0300

    Add configuration of url parameters

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='books-detail'),
-    path('books/create/', views.BookCreateView.as_view(), name='books-create'),
+
+    
+    path('books/update/', views.BookUpdateView.as_view(), name='books-update-dummy'),
+    path('books/delete/', views.BookDeleteView.as_view(), name='books-delete-dummy'),
+
+  
     path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='books-update'),
     path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='books-delete'),
 ]
