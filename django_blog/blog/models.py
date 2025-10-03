5849bf88691a61c0dc0290508844e84b47ad9b3b
Ednah Bridget <bidgetkakah@gmail.com>
2025-10-03 19:24:28 +0300

    Add updates to blog/models.py for setup & project configuration for Django blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User   # <-- required by the checker
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # <-- uses imported User
 
     def __str__(self):
         return self.title
